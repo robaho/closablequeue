545a0d76afaf09a7a891f206d185be7efe893fed
robert engels <robaho@users.noreply.github.com>
2024-07-25 10:38:41 -0500

    add blocking drainTo

--- lib/src/main/java/robaho/queue/ClosableQueue.java
+++ lib/src/main/java/robaho/queue/ClosableQueue.java
@@ -2,6 +2,7 @@ package robaho.queue;
 
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -115,6 +116,33 @@ public class ClosableQueue<T> implements AutoCloseable {
             lock.unlock();
         }
     }
+
+    /**
+     * Drain all of the elements of the queue up to maxElements into the provided collection. If the queue is empty, the method returns immediately.
+     * @param c is the non-null Collection to receive the elements.
+     * @param maxElements is the maximum number of elements to drain.
+     * @throws IllegalStateException if the queue is closed.
+     */
+    public int drainToBlocking(Collection<? super T> c) throws InterruptedException {
+        lock.lock();
+        try {
+            while (true) { 
+                if(closed) throw new IllegalStateException("queue is closed");
+                if(list.isEmpty()) {
+                    condition.await();
+                } else {
+                    int count=0;
+                    for(T e;(e=list.poll())!=null;count++) {
+                        c.add(e);
+                    }
+                    return count;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
 
     /**
      * Remove earliest element from the queue and return it, blocking until an element is available.
